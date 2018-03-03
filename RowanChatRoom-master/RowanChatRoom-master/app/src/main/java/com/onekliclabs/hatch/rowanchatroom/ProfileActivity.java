0f0ea69b997d8e8308f1b76cf248475883dc8d5a
darwint159 <silversoul159@gmail.com>
2018-03-02 19:10:42 -0500

    Profile Page layout Changes 2.0

---
+++ RowanChatRoom-master/RowanChatRoom-master/app/src/main/java/com/onekliclabs/hatch/rowanchatroom/ProfileActivity.java
@@ -0,0 +1,21 @@
+package com.onekliclabs.hatch.rowanchatroom;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by Owner on 7/19/15.
+ */
+public class ProfileActivity extends Activity
+{
+}
+
